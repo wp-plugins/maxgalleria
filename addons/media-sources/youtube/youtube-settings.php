842ffe06813beb6b60e617ebf403c30f617b1c23
arcware <arcware@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-08 17:19:39 +0000

    Tag for 2.1.0
    git-svn-id: https://plugins.svn.wordpress.org/maxgalleria/tags/2.1.0@910631 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ addons/media-sources/youtube/youtube-settings.php
@@ -0,0 +1,11 @@
+<div class="settings-title">
+	<?php _e('YouTube Settings', 'maxgalleria') ?>
+</div>
+
+<div class="settings-options">
+	<p><?php _e('There are no YouTube settings at this time.', 'maxgalleria') ?></p>
+	
+	<form id="form-youtube-settings">
+
+	</form>
+</div>
